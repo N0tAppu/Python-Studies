3a632a6f05b01e4f57aa1814b7fda595cfaca5e3
Sreehari S <sreeharisisilal2007@gamil.com>
2026-03-14 21:55:40 +0530

    A Simple Bonus Salary Calc

---
+++ Exam Ques Practice.py
@@ -0,0 +1,24 @@
+'''
+Develop a Python program that accepts an employee’s basic salary as input and
+calculates their bonus based on the following conditions:
+i. If the salary is above 50,000, the bonus is 15% of the salary.
+ii. If the salary is between 30,000 and 50,000, the bonus is 10% of the
+salary.
+iii. If the salary is below 30,000, the bonus is 5% of the salary.
+The program should print the total salary including the bonus.
+'''
+name = str(input("Enter Employee Name:"))
+salary = float(input(f"Enter Salary of {name}: "))
+print()
+print("Employee:    ",name)
+print("Salary:      ",round(salary,2))
+if salary > 50000:
+    bonus = 0.15
+elif 30000< salary <= 50000:
+    bonus = 0.1
+elif 0< salary <=30000:
+    bonus = 0.05
+elif salary <=0 or salary >50000:
+    print("Salary out of range!")
+total_salary = round(salary+(salary*bonus),2)
+print(f"Total Salary of {name} including Bonus is: {total_salary}")
